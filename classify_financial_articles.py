01e14f28b906b6643abe2cc2d5aa46e49e8562f0
StrategicalIT <richard+github@thekiwis.net>
2022-06-27 13:56:04 +1000

    python v5

--- classify_financial_articles.py
+++ classify_financial_articles.py
@@ -7,9 +7,9 @@ tfidf_vectorizer = pickle.load(open('financial_text_vectorizer.pkl', 'rb'))
 label_encoder = pickle.load(open('financial_text_encoder.pkl', 'rb'))
 
 
-def process(inpath, outpath):
+def process(inPath, outPath):
     # read the file
-    input_df = pd.read_csv(inpath)
+    input_df = pd.read_csv(inPath)
     # vectorize the input
     features = tfidf_vectorizer.transform(input_df['body'])
     # predict the classes
@@ -18,7 +18,7 @@ def process(inpath, outpath):
     input_df['category'] = label_encoder.inverse_transform(predictions)
     # save the predictions
     output_df = input_df[['id', 'category']]
-    output_df.to_csv(outpath, index=False)
+    output_df.to_csv(outPath, index=False)
 
-    #pass the funtion into the Gravity-AI helper
-    grav.wait_for_requests(process)
+#pass the funtion into the Gravity-AI helper
+grav.wait_for_requests(process)

58e19a93d90e874f83afc43e9d221dfe8a8c2ef3
StrategicalIT <richard+github@thekiwis.net>
2022-06-27 12:10:57 +1000

    Fix another syntax error in Pyton :-)

--- classify_financial_articles.py
+++ classify_financial_articles.py
@@ -3,14 +3,14 @@ import pickle
 import pandas as pd
 
 model = pickle.load(open('financial_text_classifier.pkl', 'rb' ))
-tfidf_vectorsizer = pickle.load(open('financial_text_vectorizer.pkl', 'rb' ))
+tfidf_vectorizer = pickle.load(open('financial_text_vectorizer.pkl', 'rb' ))
 label_encoder = pickle.load(open('financial_text_encoder.pkl', 'rb' ))
 
 def process(inpath, outpath):
     # read the file
     input_df = pd.read_csv(inpath)
     # vectorize the input
-    features = tfidf_vectorsizer.transform(input_df['body'])
+    features = tfidf_vectorizer.transform(input_df['body'])
     # predict the classes
     predictions = model.predict(features)
     # convert the predictions to categories
@@ -19,5 +19,5 @@ def process(inpath, outpath):
     output_df = input_df[['id', 'category']]
     output_df.to_csv(outpath, index=False)
 
-    #grav.wait_for_request(process)
+    #pass the funtion into the Gravity-AI helper
     grav.wait_for_requests(process)
